b5b88fb6aff1b6f390f664ddd4de73e87b459264
SKILLFULL Java-User <39699356+htl-steinbej@users.noreply.github.com>
2018-06-11 13:49:00 +0200

    Update MainGUI.java

--- ForDeletion/src/pkgGUI/MainGUI.java
+++ ForDeletion/src/pkgGUI/MainGUI.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class MainGUI extends JFrame {
+	private JButton btnOK;/*MASTER*/
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	public static void main(String[] args) {
